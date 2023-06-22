669bb088881bd2a409224f104358f18664375e29
myroslava-kukhta <miroslavakukhta@gmail.com>
2023-06-22 16:23:04 +0300

    first_react_app

---
+++ my-app/src/components/main.jsx
@@ -0,0 +1,26 @@
+const Main = () => {
+  return (
+    <div className='app-wrapper'> 
+      <header className='header'>
+        <img src='https://cdn.freelogodesign.org/files/38c2f753288c45789f8b7a6172aa27f3/thumb/logo_200x200.png?v=638230353640000000'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          Profile
+        </div>
+        <div>
+          Messages
+        </div>
+        <div>
+          News
+        </div>
+        <div>Settings</div>
+      </nav>
+      <div className='content'>
+        Main content
+      </div>
+    </div>
+  )
+}
+
+export default Main;
